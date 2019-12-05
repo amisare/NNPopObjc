1cd66bde83128968cb3778d70d28eb373ff0a8ac
HaiJun Gu <243297288@qq.com>
2019-12-05 15:34:16 +0800

    modify: igonred -Wshadow warning

--- NNPopObjc/Public/NNPopObjcScope.h
+++ NNPopObjc/Public/NNPopObjcScope.h
@@ -14,15 +14,18 @@
  */
 #define nn_pop_exscope(VAR) \
 		nn_pop_scope_keywordify \
-		__typeof__(VAR) nn_pop_metamacro_concat(VAR, _scope_) = (VAR); \
+		__typeof__(VAR) nn_pop_metamacro_concat(VAR, _scope_) = (VAR);
 
 /**
  * internal scope
  */
 #define nn_pop_inscope(TYPE, VAR) \
 		nn_pop_scope_keywordify \
-		TYPE VAR = (TYPE)nn_pop_metamacro_concat(VAR, _scope_);
-
+		_Pragma("clang diagnostic push") \
+		_Pragma("clang diagnostic ignored \"-Wshadow\"") \
+		TYPE VAR = (TYPE)nn_pop_metamacro_concat(VAR, _scope_); \
+		_Pragma("clang diagnostic pop")
+		
 
 #if defined(DEBUG) && !defined(NDEBUG)
 #define nn_pop_scope_keywordify autoreleasepool {}
